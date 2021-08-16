a0c006fed25d077c3268bd1184fc955439ab1b66
Edwards Silvera <edwards.silvera@gmail.com>
2021-08-16 15:45:31 +0000

    Ejercicio Excuse Generator Finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let button = document.getElementById("boton");
+  let respuesta = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  button.addEventListener("click", function() {
+    let mensaje = `${who[Math.floor(Math.random() * who.length)]} ${
+      action[Math.floor(Math.random() * action.length)]
+    } ${what[Math.floor(Math.random() * what.length)]} ${
+      when[Math.floor(Math.random() * when.length)]
+    }`;
+    respuesta.innerHTML = mensaje;
+  });
 };
